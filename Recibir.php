6868cbb5f8c654b2068733290838c1800dc56fda
ecr007 <evercuevas1000@gmail.com>
2014-05-13 06:32:37 -0700

    Create Recibir.php

---
+++ Recibir.php
@@ -0,0 +1,10 @@
+/*Esta funcion es para imprimir values traidos desde el web service by ECR*/
+function imprime_value_of_rest($url){
+	$json = file_get_contents($url);
+    $contenido = json_decode($json, true);
+
+    for ($i=0; $i < count($contenido) ; $i++){
+        //Cuerpo de lo que imprimo
+        echo "<option value='".$contenido[$i]['id']."'>".$contenido[$i]['name']."</option>";
+    }
+}//FIN
